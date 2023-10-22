e4d0bc9d92808e8bbddfc684fbb3214c67fc4a84
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2023-10-22 10:32:32 +0200

    Ajout des variables réactives

---
+++ Assets/com.nottwice.tools/Runtime/ScriptableObjects/Variables/Typed/Reactives/ReactiveFloatVariable.cs
@@ -0,0 +1,11 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Assets.NotTwice.Tools.Runtime.ScriptableObjects.Variables.Typed.Reactives
+{
+	[CreateAssetMenu(fileName = "ReactiveFloatVariable", menuName = "NotTwice/Tools/Variables/Reactives/ReactiveFloatVariable")]
+	public class ReactiveFloatVariable : ScriptableObject
+	{
+		public FloatReactiveProperty Value;
+	}
+}
---
+++ Assets/com.nottwice.tools/Runtime/ScriptableObjects/Variables/Typed/Reactives/ReactiveVector2Variable.cs
@@ -0,0 +1,11 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Assets.NotTwice.Tools.Runtime.ScriptableObjects.Variables.Typed.Reactives
+{
+	[CreateAssetMenu(fileName = "ReactiveVector2Variable", menuName = "NotTwice/Tools/Variables/Reactives/ReactiveVector2Variable")]
+	public class ReactiveVector2Variable : ScriptableObject
+	{
+		public Vector2ReactiveProperty Value;
+	}
+}
