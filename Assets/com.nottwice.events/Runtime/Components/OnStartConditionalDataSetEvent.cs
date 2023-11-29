6cf18fafa4ac790d0eb79888be134465d5a5bab1
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2023-11-29 18:16:22 +0100

    Add components for events package

---
+++ Assets/com.nottwice.events/Runtime/Components/OnStartConditionalDataSetEvent.cs
@@ -0,0 +1,33 @@
+﻿using Assets.com.nottwice.events.Runtime.ScriptableObjects;
+using Assets.com.nottwice.events.Runtime.Serializables;
+using Assets.com.nottwice.scriptableobjects.Runtime.Collections;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace Assets.com.nottwice.events.Runtime.Components
+{
+	/// <summary>
+	/// Abstract class used to trigger a conditioned event when the component is activated.
+	/// The event triggered is the one linked to the conditioned item.
+	/// </summary>
+	public abstract class OnStartConditionalDataSetEvent<T> : MonoBehaviour
+	{
+		[Required, Tooltip("Event to register with.")]
+		public ScriptableDataSet<DataEventItem<T>> DataEventItems;
+
+		public T ConditionalItem;
+
+		public void OnEnable()
+		{
+			GameEvent targetEvent = GetTargetEvent();
+
+			ApplicationInstancesContainer.Logger.Log(LogType.Log, $"Start for {name} with event {targetEvent.name}");
+			targetEvent.Raise();
+		}
+
+		protected GameEvent GetTargetEvent()
+		{
+			return DataEventItems.DataSet.Find(dei => dei.IsSameItem(ConditionalItem)).Event;
+		}
+	}
+}
---
+++ Assets/com.nottwice.events/Runtime/Serializables/DataEventItem.cs
@@ -0,0 +1,24 @@
+﻿using Assets.com.nottwice.events.Runtime.ScriptableObjects;
+
+namespace Assets.com.nottwice.events.Runtime.Serializables
+{
+	/// <summary>
+	/// Abstract class for use with a T model to link data to an event
+	/// </summary>
+	public abstract class DataEventItem<T>
+	{
+		public GameEvent Event;
+
+		public T Item;
+
+		public void Raise()
+		{
+			Event?.Raise();
+		}
+
+		public bool IsSameItem(T item)
+		{
+			return Item.Equals(item);
+		}
+	}
+}
