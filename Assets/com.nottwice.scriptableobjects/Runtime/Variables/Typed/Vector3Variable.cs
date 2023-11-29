d24a44879a4b5f98e70219b00f71161dabeda96d
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2023-11-29 23:14:30 +0100

    Add vector variables

---
+++ Assets/com.nottwice.scriptableobjects/Runtime/Variables/Typed/Vector3Variable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.com.nottwice.scriptableobjects.Runtime.Variables.Typed
+{
+	[CreateAssetMenu(fileName = "Vector3Variable", menuName = "NotTwice/Variables/Vector3Variable")]
+	public class Vector3Variable : ScriptableVariable<Vector3>
+	{
+	}
+}
