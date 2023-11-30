47d3b804cd251a507d8a83547175878eb046e11a
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2023-11-30 13:10:58 +0100

    update ui package

---
+++ Assets/com.nottwice.ui/Runtime/Components/ImageChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.com.nottwice.ui.Runtime.Components
+{
+	/// <summary>
+	/// Component for changing the sprite value of an image component from an other source
+	/// </summary>
+	[AddComponentMenu("NotTwice/UI/ImageChanger")]
+	[DisallowMultipleComponent]
+	public class ImageChanger : MonoBehaviour
+	{
+		public Image ImageComponent;
+
+		public void ChangeFromSpriteSource(Sprite sprite)
+		{
+			ApplicationInstancesContainer.Logger.Log(LogType.Log, $"New sprite for component {ImageComponent.name}: {sprite.name}");
+
+			ImageComponent.sprite = sprite;
+		}
+	}
+}
