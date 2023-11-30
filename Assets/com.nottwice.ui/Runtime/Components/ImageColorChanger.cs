64d2d528c18c02eaf12cee4f512025b1f5b8315a
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2023-11-30 14:20:52 +0100

    add ui components

---
+++ Assets/com.nottwice.ui/Runtime/Components/ImageColorChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.com.nottwice.ui.Runtime.Components
+{
+	/// <summary>
+	/// Component for changing the color value of an image component from an other source
+	/// </summary>
+	[AddComponentMenu("NotTwice/UI/ImageColorChanger")]
+	[DisallowMultipleComponent]
+	public class ImageColorChanger : MonoBehaviour
+	{
+		public Image ImageComponent;
+
+		public void ChangeFromSpriteSource(Color color)
+		{
+			ApplicationInstancesContainer.Logger.Log(LogType.Log, $"New color for component {ImageComponent.name}: {color}");
+
+			ImageComponent.color = color;
+		}
+	}
+}
---
+++ Assets/com.nottwice.ui/Runtime/Components/SpriteRendererColorChanger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.com.nottwice.ui.Runtime.Components
+{
+	/// <summary>
+	/// Component for changing the sprite value of an spriterenderer component from an other source
+	/// </summary>
+	[AddComponentMenu("NotTwice/UI/SpriteRendererColorChanger")]
+	[DisallowMultipleComponent]
+	public class SpriteRendererColorChanger : MonoBehaviour
+	{
+		public SpriteRenderer SpriteRendererComponent;
+
+		public void ChangeFromSpriteSource(Color color)
+		{
+			ApplicationInstancesContainer.Logger.Log(LogType.Log, $"New color for component {SpriteRendererComponent.name}: {color}");
+
+			SpriteRendererComponent.color = color;
+		}
+	}
+}
