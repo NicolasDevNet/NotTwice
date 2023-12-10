7879365d4193da5c269a908f1a6e85d6596168c2
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2023-12-10 13:03:35 +0100

    add color variable

---
+++ Assets/com.nottwice.scriptableobjects/Runtime/Variables/Typed/ColorVariable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.com.nottwice.scriptableobjects.Runtime.Variables.Typed
+{
+	[CreateAssetMenu(fileName = "ColorVariable", menuName = "NotTwice/Variables/ColorVariable")]
+	public class ColorVariable : ScriptableVariable<Color>
+	{
+	}
+}
