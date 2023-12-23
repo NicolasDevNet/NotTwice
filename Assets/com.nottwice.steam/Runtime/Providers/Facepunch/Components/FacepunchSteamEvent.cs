eb812319fa3914597a66614d551688acbc760916
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2023-12-23 11:24:32 +0100

    fix steam package

--- Assets/com.nottwice.steam/Runtime/Providers/Facepunch/Components/FacepunchSteamEvent.cs
+++ Assets/com.nottwice.steam/Runtime/Providers/Facepunch/Components/FacepunchSteamEvent.cs
@@ -1,9 +1,11 @@
 ﻿using Steamworks;
 using Steamworks.Data;
+using System;
 using UnityEngine.Events;
 
 namespace Assets.com.nottwice.steam.Runtime.Providers.Facepunch.Components
 {
+	[Serializable]
 	public class FacepunchSteamEvent : UnityEvent<Lobby, Friend>
 	{
 	}
