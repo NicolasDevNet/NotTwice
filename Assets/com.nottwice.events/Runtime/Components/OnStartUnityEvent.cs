1d415c25193fdc611ccfcb991060de6eb606c1c5
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2024-01-11 13:40:39 +0100

    update event pckg

---
+++ Assets/com.nottwice.events/Runtime/Components/OnStartUnityEvent.cs
@@ -0,0 +1,24 @@
+﻿using Assets.com.nottwice.events.Runtime.ScriptableObjects;
+using Assets.com.nottwice.lifetime.Runtime;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.com.nottwice.events.Runtime.Components
+{
+	/// <summary>
+	/// Component to trigger a UnityEvent when a Start event is detected.
+	/// </summary>
+	[DefaultExecutionOrder(12)]
+	[AddComponentMenu("NotTwice/Events/OnStartUnityEvent")]
+	public class OnStartUnityEvent : MonoBehaviour
+	{
+		[Required, Tooltip("Event to register with.")]
+		public UnityEvent Event;
+
+		public void Start()
+		{
+			Event?.Invoke();
+		}
+	}
+}
