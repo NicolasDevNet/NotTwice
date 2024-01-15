99c2a8235526baf8eb13dbef0d737a6bd56f9405
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2024-01-15 15:10:21 +0100

    update components pckg

---
+++ Assets/com.nottwice.components/Runtime/UI/CollectionContainer.cs
@@ -0,0 +1,21 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace Assets.com.nottwice.components.Runtime.UI
+{
+
+	/// <summary>
+	/// Component for making available the children of the gameobject associated with the same type T
+	/// </summary>
+	public abstract class CollectionContainer<T> : MonoBehaviour
+		where T : Component
+	{
+		[ReadOnly]
+		public T[] Children;
+
+		public void Awake()
+		{
+			Children = GetComponentsInChildren<T>();
+		}
+	}
+}
