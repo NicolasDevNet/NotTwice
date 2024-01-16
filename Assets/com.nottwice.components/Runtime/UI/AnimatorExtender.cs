78884a84350129c6672bc4df2628cf54a4592e39
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2024-01-16 14:52:14 +0100

    update

---
+++ Assets/com.nottwice.components/Runtime/UI/AnimatorExtender.cs
@@ -0,0 +1,27 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace Assets.com.nottwice.components.Runtime.UI
+{
+	[AddComponentMenu("NotTwice/UI/AnimatorExtender")]
+	[DisallowMultipleComponent]
+	[RequireComponent(typeof(Animator))]
+	public class AnimatorExtender : MonoBehaviour
+	{
+		[ReadOnly, Tooltip("The animator is fed automatically if there is an animator on the same gameobject.")]
+		public Animator Animator;
+
+		public void Awake()
+		{
+			Animator = GetComponent<Animator>();
+		}
+
+		public bool PlayingAnimationIsDone
+		{
+			get
+			{
+				return Animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f;
+			}
+		}
+	}
+}
