2cff7c5e0e2dc19eaa98a506f59fde841b60ed04
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2024-01-16 13:46:43 +0100

    update event pckg

---
+++ Assets/com.nottwice.events/Runtime/Components/EventFlowManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.com.nottwice.events.Runtime.Components
+{
+	[AddComponentMenu("NotTwice/Events/EventFlowManager")]
+	[DisallowMultipleComponent]
+	public class EventFlowManager : MonoBehaviour
+	{
+		public List<EventFlowPart> FlowParts = new List<EventFlowPart>();
+
+		public void Execute()
+		{
+			foreach(EventFlowPart part in FlowParts)
+			{
+				StartCoroutine(part.Execute());
+			}
+		}
+	}
+}
---
+++ Assets/com.nottwice.events/Runtime/Components/EventFlowPart.cs
@@ -0,0 +1,22 @@
+﻿using NaughtyAttributes;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.com.nottwice.events.Runtime.Components
+{
+	[AddComponentMenu("NotTwice/Events/EventFlowPart")]
+	public class EventFlowPart : MonoBehaviour
+	{
+		[ReadOnly]
+		public bool IsDone;
+
+		public UnityEvent<EventFlowPart> Event;
+
+		public IEnumerator Execute()
+		{
+			Event?.Invoke(this);
+			yield return new WaitUntil(() => IsDone);
+		}
+	}
+}
