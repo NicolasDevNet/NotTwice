d4f9cb3f639ceff4ced6e1ff0df0fe89771089d6
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2024-01-22 10:12:28 +0100

    update event pckg

---
+++ Assets/com.nottwice.events/Runtime/Components/EventBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.com.nottwice.events.Runtime.Components
+{
+	[AddComponentMenu("NotTwice/Events/EventBehaviour")]
+	[DisallowMultipleComponent]
+	public class EventBehaviour : MonoBehaviour
+	{
+		public UnityEvent UnityEvent;
+
+		public void Raise()
+		{
+			UnityEvent?.Invoke();
+		}
+	}
+}
