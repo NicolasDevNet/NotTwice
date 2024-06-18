94bfcd9d224c107e7c6370bc6762260001f80a37
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2024-06-18 15:09:33 +0200

    Update event package

--- Assets/com.nottwice.events/Runtime/Components/Flow/NTEventFlowPart.cs
+++ Assets/com.nottwice.events/Runtime/Components/Flow/NTEventFlowPart.cs
@@ -1,7 +1,7 @@
 ﻿using NaughtyAttributes;
 using NotTwice.Events.Runtime.ScriptableObjects.Events;
+using NotTwice.Events.Runtime.Serializables.Abstract;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace NotTwice.Events.Runtime.Components.Flow
 {
@@ -9,43 +9,13 @@ namespace NotTwice.Events.Runtime.Components.Flow
     /// Part of a managed flow <see cref="NTEventFlowManager"/>
     /// </summary>
     [AddComponentMenu("NotTwice/Events/Flow/EventFlowPart")]
-	public class NTEventFlowPart : MonoBehaviour
+	public class NTEventFlowPart : NTGenericEventTypeSwitcher<NTFlowPartGameEvent, NTEventFlowPart>
 	{
-        /// <summary>
-        /// Defines whether a UnityEvent object should be used to trigger the event
-        /// </summary>
-        [OnValueChanged(nameof(OnUseUnityEventChange))]
-        [Tooltip("Defines whether a UnityEvent object should be used to trigger the event")]
-        public bool UseUnityEvent;
-
-        /// <summary>
-        /// Defines whether a GameEvent object should be used to trigger the event
-        /// </summary>
-        [OnValueChanged(nameof(OnUseGameEventChange))]
-        [Tooltip("Defines whether a GameEvent object should be used to trigger the event")]
-        public bool UseGameEvent;
-
         /// <summary>
         /// Flag indicating the status of this flow element
         /// </summary>
 		[ReadOnly, Tooltip("Flag indicating the status of this flow element")]
 		public bool IsDone;
-
-        /// <summary>
-        /// Unity event triggered for this flow element
-        /// <para>The value of this instance of <see cref="NTEventFlowPart"/> is passed as a parameter to manipulate its status later.</para>
-        /// </summary>
-        [ShowIf(nameof(UseUnityEvent))]
-        [Tooltip("Unity event triggered for this flow element")]
-		public UnityEvent<NTEventFlowPart> UnityEvent;
-
-        /// <summary>
-        /// Game event triggered for this flow element
-        /// <para>The value of this instance of <see cref="NTEventFlowPart"/> is passed as a parameter to manipulate its status later.</para>
-        /// </summary>
-        [ShowIf(nameof(UseGameEvent))]
-        [Tooltip("Game event triggered for this flow element")]
-        public NTFlowPartGameEvent GameEvent;
 
         /// <summary>
         /// Unity Editor Friendly method for indicating that this flow element has completed its task
@@ -60,30 +30,7 @@ namespace NotTwice.Events.Runtime.Components.Flow
         /// </summary>
 		public void Execute()
 		{
-            if (UseUnityEvent)
-            {
-                UnityEvent?.Invoke(this);
-            }
-            else if(UseGameEvent)
-            {
-                GameEvent?.Raise(this);
-            }
+            Raise(this);
 		}
-
-        private void OnUseUnityEventChange()
-        {
-            if(UseUnityEvent)
-            {
-                UseGameEvent = false;
-            }
-        }
-
-        private void OnUseGameEventChange()
-        {
-            if (UseGameEvent)
-            {
-                UseUnityEvent = false;
-            }
-        }
     }
 }
---
+++ Assets/com.nottwice.events/Runtime/Serializables/Abstract/NTGenericEventTypeSwitcher.cs
@@ -0,0 +1,68 @@
+﻿using NaughtyAttributes;
+using NotTwice.Events.Runtime.ScriptableObjects.Abstract;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace NotTwice.Events.Runtime.Serializables.Abstract
+{
+	/// <summary>
+	/// Classe de base permettant à celles qui l'implémente de choisir entre un évènement
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <typeparam name="U"></typeparam>
+	public abstract class NTGenericEventTypeSwitcher<T, U>
+		where T :  NTGenericGameEvent<T, U>
+	{
+		/// <summary>
+		/// Defines whether a Unity event is to be used
+		/// </summary>
+		[Tooltip("Defines whether a Unity event is to be used") ,OnValueChanged(nameof(OnUseUnityEventChanged))]
+		public bool UseUnityEvent;
+
+		/// <summary>
+		/// Defines whether a Game event is to be used
+		/// </summary>
+		[Tooltip("Defines whether a Game event is to be used") , OnValueChanged(nameof(OnUseGameEventChanged))]
+		public bool UseGameEvent;
+
+		/// <summary>
+		/// The scriptable event object to use
+		/// </summary>
+		[Tooltip("The scriptable event object to use"), ShowIf(nameof(UseGameEvent))]
+		public T GameEvent;
+
+		/// <summary>
+		/// The unity event to use
+		/// </summary>
+		[Tooltip("The unity event to use"), ShowIf(nameof(UseUnityEvent))]
+		public UnityEvent<U> UnityEvent;
+
+		public void Raise(U value)
+		{
+			if(UseUnityEvent)
+			{
+				UnityEvent?.Invoke(value);
+			}
+			else
+			{
+				GameEvent?.Raise(value);
+			}
+		}
+
+		private void OnUseGameEventChanged()
+		{
+			if (UseGameEvent)
+			{
+				UseUnityEvent = false;
+			}
+		}
+
+		private void OnUseUnityEventChanged()
+		{
+			if (UseUnityEvent)
+			{
+				UseGameEvent = false;
+			}
+		}
+	}
+}
