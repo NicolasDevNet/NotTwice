981a6d3467f06b4081182b3724db9feb0476e18e
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2024-06-26 11:42:10 +0200

    Add application proxy

---
+++ Assets/com.nottwice.proxies/Runtime/Interfaces/INTApplication.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace NotTwice.Proxies.Runtime.Interfaces
+{
+	/// <summary>
+	/// Interface contract outlining methods to be defined for unity application operations
+	/// </summary>
+	public interface INTApplication
+	{
+		string AbsoluteURL { get; }
+		ThreadPriority BackgroundLoadingPriority { get; set; }
+		string BuildGUID { get; }
+		string CloudProjectId { get; }
+		string CompanyName { get; }
+		string ConsoleLogPath { get; }
+		string DataPath { get; }
+		bool Genuine { get; }
+		bool GenuineCheckAvailable { get; }
+		string Identifier { get; }
+		string InstallerName { get; }
+		ApplicationInstallMode InstallMode { get; }
+		NetworkReachability InternetReachability { get; }
+		bool IsBatchMode { get; }
+		bool IsConsolePlatform { get; }
+		bool IsEditor { get; }
+		bool IsFocused { get; }
+		bool IsMobilePlatform { get; }
+		bool IsPlaying { get; }
+		string PersistentDataPath { get; }
+		RuntimePlatform Platform { get; }
+		string ProductName { get; }
+		bool RunInBackground { get; set; }
+		ApplicationSandboxType SandboxType { get; }
+		string StreamingAssetsPath { get; }
+		SystemLanguage SystemLanguage { get; }
+		int TargetFrameRate { get; set; }
+		string TemporaryCachePath { get; }
+		string UnityVersion { get; }
+		string Version { get; }
+
+		void AddDeepLinkActivatedAction(Action<string> action);
+		void AddFocusChangedAction(Action<bool> action);
+		void AddLogMessageReceivedAction(Application.LogCallback action);
+		void AddLogMessageReceivedThreadedAction(Application.LogCallback action);
+		void AddLowMemoryAction(Application.LowMemoryCallback action);
+		void AddOnBeforeRenderAction(UnityAction action);
+		void AddQuittingAction(Action action);
+		void AddUnloadingAction(Action action);
+		void AddWantsToQuitAction(Func<bool> func);
+		bool CanStreamedLevelBeLoaded(int levelIndex);
+		bool CanStreamedLevelBeLoaded(string levelName);
+		string[] GetBuildTags();
+		StackTraceLogType GetStackTraceLogType(LogType logType);
+		bool HasProLicense();
+		bool HasUserAuthorization(UserAuthorization userAuthorization);
+		bool IsObjPlaying(UnityEngine.Object @object);
+		void OpenURL(string url);
+		void Quit();
+		void Quit(int exitCode);
+		void RemoveDeepLinkActivatedAction(Action<string> action);
+		void RemoveFocusChangedAction(Action<bool> action);
+		void RemoveLogMessageReceivedAction(Application.LogCallback action);
+		void RemoveLogMessageReceivedThreadedAction(Application.LogCallback action);
+		void RemoveLowMemoryAction(Application.LowMemoryCallback action);
+		void RemoveOnBeforeRenderAction(UnityAction action);
+		void RemoveQuittingAction(Action action);
+		void RemoveUnloadingAction(Action action);
+		void RemoveWantsToQuitAction(Func<bool> func);
+		bool RequestAdvertisingIdentifierAsync(Application.AdvertisingIdentifierCallback callback);
+		AsyncOperation RequestUserAuthorization(UserAuthorization userAuthorization);
+		void SetBuildTags(string[] buildTags);
+		void SetStackTraceLogType(LogType logType, StackTraceLogType stackTraceLogType);
+		void Unload();
+	}
+}
---
+++ Assets/com.nottwice.proxies/Runtime/NTApplicationProxy.cs
@@ -0,0 +1,443 @@
+﻿using NotTwice.Proxies.Runtime.Interfaces;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using static UnityEngine.Application;
+
+namespace Assets.com.nottwice.proxies.Runtime
+{
+	/// <summary>
+	/// Proxy acting as a flat pass for <see cref="Application"/> static methods.
+	/// </summary>
+	public class NTApplicationProxy : INTApplication
+    {
+        #region Properties
+
+        public string AbsoluteURL 
+        {
+            get
+            {
+                return absoluteURL;
+            }
+        }
+
+        public ThreadPriority BackgroundLoadingPriority
+        {
+            get
+            {
+                return backgroundLoadingPriority;
+            }
+            set
+            {
+                backgroundLoadingPriority = value;
+            }
+        }
+
+        public string BuildGUID
+        {
+            get
+            {
+                return buildGUID;
+            }
+        }
+
+        public string CloudProjectId
+        {
+            get
+            {
+                return cloudProjectId;
+            }
+        }
+
+        public string CompanyName
+        {
+            get
+            {
+                return companyName;
+            }
+        }
+
+        public string ConsoleLogPath
+        {
+            get
+            {
+                return consoleLogPath;
+            }
+        }
+
+        public string DataPath
+        {
+            get
+            {
+                return dataPath;
+            }
+        }
+
+        public bool Genuine
+        {
+            get
+            {
+                return genuine;
+            }
+        }
+
+        public bool GenuineCheckAvailable
+        {
+            get
+            {
+                return genuineCheckAvailable;
+            }
+        }
+
+        public string Identifier
+        {
+            get
+            {
+                return identifier;
+            }
+        }
+
+        public string InstallerName
+        {
+            get
+            {
+                return installerName;
+            }
+        }
+
+        public ApplicationInstallMode InstallMode
+        {
+            get
+            {
+                return installMode;
+            }
+        }
+
+        public NetworkReachability InternetReachability
+        {
+            get
+            {
+                return internetReachability;
+            }
+        }
+
+        public bool IsBatchMode
+        {
+            get
+            {
+                return isBatchMode;
+            }
+        }
+
+        public bool IsConsolePlatform
+        {
+            get
+            {
+                return isConsolePlatform;
+            }
+        }
+
+        public bool IsEditor
+        {
+            get
+            {
+                return isEditor;
+            }
+        }
+
+        public bool IsFocused
+        {
+            get
+            {
+                return isFocused;
+            }
+        }
+
+        public bool IsMobilePlatform
+        {
+            get
+            {
+                return isMobilePlatform;
+            }
+        }
+
+        public bool IsPlaying
+        {
+            get
+            {
+                return isPlaying;
+            }
+        }
+
+        public string PersistentDataPath
+        {
+            get
+            {
+                return persistentDataPath;
+            }
+        }
+
+        public RuntimePlatform Platform
+        {
+            get
+            {
+                return platform;
+            }
+        }
+
+        public string ProductName
+        {
+            get
+            {
+                return productName;
+            }
+        }
+
+        public bool RunInBackground
+        {
+            get
+            {
+                return runInBackground;
+            }
+            set
+            {
+                runInBackground = value;
+            }
+        }
+
+        public ApplicationSandboxType SandboxType
+        {
+            get
+            {
+                return sandboxType;
+            }
+        }
+
+        public string StreamingAssetsPath
+        {
+            get
+            {
+                return streamingAssetsPath;
+            }
+        }
+
+        public SystemLanguage SystemLanguage
+        {
+            get
+            {
+                return systemLanguage;
+            }
+        }
+
+        public int TargetFrameRate
+        {
+            get
+            {
+                return targetFrameRate;
+            }
+            set
+            {
+                targetFrameRate = value;
+            }
+        }
+
+        public string TemporaryCachePath
+        {
+            get
+            {
+                return temporaryCachePath;
+            }
+        }
+
+        public string UnityVersion
+        {
+            get
+            {
+                return unityVersion;
+            }
+        }
+
+        public string Version
+        {
+            get
+            {
+                return version;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        #region Delegate accessors
+
+        public void AddQuittingAction(Action action)
+        {
+            quitting += action;
+        }
+
+        public void RemoveQuittingAction(Action action)
+        {
+            quitting -= action;
+        }
+
+        public void AddWantsToQuitAction(Func<bool> func)
+        {
+            wantsToQuit += func;
+        }
+
+        public void RemoveWantsToQuitAction(Func<bool> func)
+        {
+            wantsToQuit -= func;
+        }
+
+        public void AddDeepLinkActivatedAction(Action<string> action)
+        {
+            deepLinkActivated += action;
+        }
+
+        public void RemoveDeepLinkActivatedAction(Action<string> action)
+        {
+            deepLinkActivated -= action;
+        }
+
+        public void AddFocusChangedAction(Action<bool> action)
+        {
+            focusChanged += action;
+        }
+
+        public void RemoveFocusChangedAction(Action<bool> action)
+        {
+            focusChanged -= action;
+        }
+
+        public void AddLogMessageReceivedAction(LogCallback action)
+        {
+            logMessageReceived += action;
+        }
+
+        public void RemoveLogMessageReceivedAction(LogCallback action)
+        {
+            logMessageReceived -= action;
+        }
+
+        public void AddLogMessageReceivedThreadedAction(LogCallback action)
+        {
+            logMessageReceivedThreaded += action;
+        }
+
+        public void RemoveLogMessageReceivedThreadedAction(LogCallback action)
+        {
+            logMessageReceivedThreaded -= action;
+        }
+
+		public void AddLowMemoryAction(LowMemoryCallback action)
+		{
+			lowMemory += action;
+		}
+
+		public void RemoveLowMemoryAction(LowMemoryCallback action)
+		{
+			lowMemory -= action;
+		}
+
+		public void AddOnBeforeRenderAction(UnityAction action)
+		{
+			onBeforeRender += action;
+		}
+
+		public void RemoveOnBeforeRenderAction(UnityAction action)
+		{
+			onBeforeRender -= action;
+		}
+
+		public void AddUnloadingAction(Action action)
+		{
+			unloading += action;
+		}
+
+		public void RemoveUnloadingAction(Action action)
+		{
+			unloading -= action;
+		}
+
+		#endregion
+
+		public bool HasUserAuthorization(UserAuthorization userAuthorization)
+        {
+            return Application.HasUserAuthorization(userAuthorization);
+        }
+
+		public bool CanStreamedLevelBeLoaded(int levelIndex)
+		{
+			return Application.CanStreamedLevelBeLoaded(levelIndex);
+		}
+
+		public bool CanStreamedLevelBeLoaded(string levelName)
+		{
+			return Application.CanStreamedLevelBeLoaded(levelName);
+		}
+
+		public string[] GetBuildTags()
+		{
+			return Application.GetBuildTags();
+		}
+
+		public StackTraceLogType GetStackTraceLogType(LogType logType)
+		{
+			return Application.GetStackTraceLogType(logType);
+		}
+
+		public bool HasProLicense()
+		{
+			return Application.HasProLicense();
+		}
+
+		public bool IsObjPlaying(UnityEngine.Object @object)
+		{
+			return Application.IsPlaying(@object);
+		}
+
+		public void OpenURL(string url)
+		{
+			Application.OpenURL(url);
+		}
+
+		public void Quit()
+		{
+			Application.Quit();
+		}
+
+		public void Quit(int exitCode)
+		{
+			Application.Quit(exitCode);
+		}
+
+		public bool RequestAdvertisingIdentifierAsync(AdvertisingIdentifierCallback callback)
+		{
+			return Application.RequestAdvertisingIdentifierAsync(callback);
+		}
+
+		public AsyncOperation RequestUserAuthorization(UserAuthorization userAuthorization)
+		{
+			return Application.RequestUserAuthorization(userAuthorization);
+		}
+
+		public void SetBuildTags(string[] buildTags)
+		{
+			Application.SetBuildTags(buildTags);
+		}
+
+		public void SetStackTraceLogType(LogType logType, StackTraceLogType stackTraceLogType)
+		{
+			Application.SetStackTraceLogType(LogType.Log, stackTraceLogType);
+		}
+
+		public void Unload()
+		{
+			Application.Unload();
+		}
+
+		#endregion
+	}
+}
