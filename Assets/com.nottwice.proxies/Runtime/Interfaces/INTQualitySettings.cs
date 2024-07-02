462c0b1ed2884297408969f42e5186b06f4f7a8a
Nicolas F <126687105+NicolasDevNet@users.noreply.github.com>
2024-07-02 09:20:23 +0200

    Ajout du proxy QualitySettings

---
+++ Assets/com.nottwice.proxies/Runtime/Interfaces/INTQualitySettings.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Rendering;
+using UnityEngine;
+
+namespace NotTwice.Proxies.Runtime.Interfaces
+{
+    /// <summary>
+	/// Interface contract outlining methods to be defined for path operations
+	/// </summary>
+    public interface INTQualitySettings
+    {
+        // Current pixel light count.
+        int PixelLightCount { get; set; }
+
+        // Shadow projection used.
+        ShadowQuality Shadows { get; set; }
+
+        // Shadow projection type.
+        ShadowProjection ShadowProjection { get; set; }
+
+        // Number of shadow cascades.
+        int ShadowCascades { get; set; }
+
+        // Maximum shadow rendering distance.
+        float ShadowDistance { get; set; }
+
+        // Default resolution of shadows.
+        ShadowResolution ShadowResolution { get; set; }
+
+        // Shadowmask mode.
+        ShadowmaskMode ShadowmaskMode { get; set; }
+
+        // Offset shadow near plane.
+        float ShadowNearPlaneOffset { get; set; }
+
+        // Split point for 2 cascade shadow.
+        float ShadowCascade2Split { get; set; }
+
+        // Split point for 4 cascade shadow.
+        Vector3 ShadowCascade4Split { get; set; }
+
+        // Desired LOD bias.
+        float LodBias { get; set; }
+
+        // Anisotropic filtering mode.
+        AnisotropicFiltering AnisotropicFiltering { get; set; }
+
+        // Master texture limit.
+        int MasterTextureLimit { get; set; }
+
+        // Maximum LOD level.
+        int MaximumLODLevel { get; set; }
+
+        // Particle raycast budget.
+        int ParticleRaycastBudget { get; set; }
+
+        // Use soft particles.
+        bool SoftParticles { get; set; }
+
+        // Use soft vegetation.
+        bool SoftVegetation { get; set; }
+
+        // VSync count.
+        int VSyncCount { get; set; }
+
+        // Anti-aliasing level.
+        int AntiAliasing { get; set; }
+
+        // Async upload time slice.
+        int AsyncUploadTimeSlice { get; set; }
+
+        // Async upload buffer size.
+        int AsyncUploadBufferSize { get; set; }
+
+        // Use persistent buffer for async upload.
+        bool AsyncUploadPersistentBuffer { get; set; }
+
+        // Use real-time reflection probes.
+        bool RealtimeReflectionProbes { get; set; }
+
+        // Billboards face camera position.
+        bool BillboardsFaceCameraPosition { get; set; }
+
+        // Resolution scaling fixed DPI factor.
+        float ResolutionScalingFixedDPIFactor { get; set; }
+
+        // Render pipeline asset.
+        RenderPipelineAsset RenderPipeline { get; set; }
+
+        // Skin weights.
+        SkinWeights SkinWeights { get; set; }
+
+        // Streaming mipmaps active.
+        bool StreamingMipmapsActive { get; set; }
+
+        // Streaming mipmaps memory budget.
+        float StreamingMipmapsMemoryBudget { get; set; }
+
+        // Streaming mipmaps renderers per frame.
+        int StreamingMipmapsRenderersPerFrame { get; set; }
+
+        // Maximum level reduction for streaming mipmaps.
+        int StreamingMipmapsMaxLevelReduction { get; set; }
+
+        // Add all cameras for streaming mipmaps.
+        bool StreamingMipmapsAddAllCameras { get; set; }
+
+        // Maximum file I/O requests for streaming mipmaps.
+        int StreamingMipmapsMaxFileIORequests { get; set; }
+
+        // Maximum queued frames.
+        int MaxQueuedFrames { get; set; }
+
+        // Quality level names.
+        string[] Names { get; }
+
+        // Desired color space.
+        ColorSpace DesiredColorSpace { get; }
+
+        // Active color space.
+        ColorSpace ActiveColorSpace { get; }
+
+        // Methods
+
+        // Increase quality level.
+        void IncreaseLevel(bool applyExpensiveChanges = false);
+
+        // Decrease quality level.
+        void DecreaseLevel(bool applyExpensiveChanges = false);
+
+        // Set quality level.
+        void SetQualityLevel(int index, bool applyExpensiveChanges = true);
+
+        // Set LOD settings.
+        void SetLODSettings(float lodBias, int maximumLODLevel, bool setDirty = true);
+
+        // Get render pipeline asset at a specific quality level.
+        RenderPipelineAsset GetRenderPipelineAssetAt(int index);
+
+        // Get current quality level.
+        int GetQualityLevel();
+
+        // Get current quality settings object.
+        Object GetQualitySettings();
+
+        // Get all render pipeline assets for a specific platform and store them in a list.
+        void GetAllRenderPipelineAssetsForPlatform(string buildTargetGroupName, ref List<RenderPipelineAsset> renderPipelineAssets);
+    }
+}
---
+++ Assets/com.nottwice.proxies/Runtime/NTQualitySettingsProxy.cs
@@ -0,0 +1,490 @@
+﻿using NotTwice.Proxies.Runtime.Interfaces;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Object = UnityEngine.Object;
+
+namespace NotTwice.Proxies.Runtime
+{
+    /// <summary>
+    /// Proxy acting as a flat pass for <see cref="QualitySettings"/> static methods.
+    /// </summary>
+    public class NTQualitySettingsProxy : INTQualitySettings
+    {
+        // Propriétés
+
+        public int PixelLightCount
+        {
+            get
+            {
+                return QualitySettings.pixelLightCount;
+            }
+            set
+            {
+                QualitySettings.pixelLightCount = value;
+            }
+        }
+
+        public ShadowQuality Shadows
+        {
+            get
+            {
+                return QualitySettings.shadows;
+            }
+            set
+            {
+                QualitySettings.shadows = value;
+            }
+        }
+
+        public ShadowProjection ShadowProjection
+        {
+            get
+            {
+                return QualitySettings.shadowProjection;
+            }
+            set
+            {
+                QualitySettings.shadowProjection = value;
+            }
+        }
+
+        public int ShadowCascades
+        {
+            get
+            {
+                return QualitySettings.shadowCascades;
+            }
+            set
+            {
+                QualitySettings.shadowCascades = value;
+            }
+        }
+
+        public float ShadowDistance
+        {
+            get
+            {
+                return QualitySettings.shadowDistance;
+            }
+            set
+            {
+                QualitySettings.shadowDistance = value;
+            }
+        }
+
+        public ShadowResolution ShadowResolution
+        {
+            get
+            {
+                return QualitySettings.shadowResolution;
+            }
+            set
+            {
+                QualitySettings.shadowResolution = value;
+            }
+        }
+
+        public ShadowmaskMode ShadowmaskMode
+        {
+            get
+            {
+                return QualitySettings.shadowmaskMode;
+            }
+            set
+            {
+                QualitySettings.shadowmaskMode = value;
+            }
+        }
+
+        public float ShadowNearPlaneOffset
+        {
+            get
+            {
+                return QualitySettings.shadowNearPlaneOffset;
+            }
+            set
+            {
+                QualitySettings.shadowNearPlaneOffset = value;
+            }
+        }
+
+        public float ShadowCascade2Split
+        {
+            get
+            {
+                return QualitySettings.shadowCascade2Split;
+            }
+            set
+            {
+                QualitySettings.shadowCascade2Split = value;
+            }
+        }
+
+        public Vector3 ShadowCascade4Split
+        {
+            get
+            {
+                return QualitySettings.shadowCascade4Split;
+            }
+            set
+            {
+                QualitySettings.shadowCascade4Split = value;
+            }
+        }
+
+        public float LodBias
+        {
+            get
+            {
+                return QualitySettings.lodBias;
+            }
+            set
+            {
+                QualitySettings.lodBias = value;
+            }
+        }
+
+        public AnisotropicFiltering AnisotropicFiltering
+        {
+            get
+            {
+                return QualitySettings.anisotropicFiltering;
+            }
+            set
+            {
+                QualitySettings.anisotropicFiltering = value;
+            }
+        }
+
+        public int MasterTextureLimit
+        {
+            get
+            {
+                return QualitySettings.masterTextureLimit;
+            }
+            set
+            {
+                QualitySettings.masterTextureLimit = value;
+            }
+        }
+
+        public int MaximumLODLevel
+        {
+            get
+            {
+                return QualitySettings.maximumLODLevel;
+            }
+            set
+            {
+                QualitySettings.maximumLODLevel = value;
+            }
+        }
+
+        public int ParticleRaycastBudget
+        {
+            get
+            {
+                return QualitySettings.particleRaycastBudget;
+            }
+            set
+            {
+                QualitySettings.particleRaycastBudget = value;
+            }
+        }
+
+        public bool SoftParticles
+        {
+            get
+            {
+                return QualitySettings.softParticles;
+            }
+            set
+            {
+                QualitySettings.softParticles = value;
+            }
+        }
+
+        public bool SoftVegetation
+        {
+            get
+            {
+                return QualitySettings.softVegetation;
+            }
+            set
+            {
+                QualitySettings.softVegetation = value;
+            }
+        }
+
+        public int VSyncCount
+        {
+            get
+            {
+                return QualitySettings.vSyncCount;
+            }
+            set
+            {
+                QualitySettings.vSyncCount = value;
+            }
+        }
+
+        public int AntiAliasing
+        {
+            get
+            {
+                return QualitySettings.antiAliasing;
+            }
+            set
+            {
+                QualitySettings.antiAliasing = value;
+            }
+        }
+
+        public int AsyncUploadTimeSlice
+        {
+            get
+            {
+                return QualitySettings.asyncUploadTimeSlice;
+            }
+            set
+            {
+                QualitySettings.asyncUploadTimeSlice = value;
+            }
+        }
+
+        public int AsyncUploadBufferSize
+        {
+            get
+            {
+                return QualitySettings.asyncUploadBufferSize;
+            }
+            set
+            {
+                QualitySettings.asyncUploadBufferSize = value;
+            }
+        }
+
+        public bool AsyncUploadPersistentBuffer
+        {
+            get
+            {
+                return QualitySettings.asyncUploadPersistentBuffer;
+            }
+            set
+            {
+                QualitySettings.asyncUploadPersistentBuffer = value;
+            }
+        }
+
+        public bool RealtimeReflectionProbes
+        {
+            get
+            {
+                return QualitySettings.realtimeReflectionProbes;
+            }
+            set
+            {
+                QualitySettings.realtimeReflectionProbes = value;
+            }
+        }
+
+        public bool BillboardsFaceCameraPosition
+        {
+            get
+            {
+                return QualitySettings.billboardsFaceCameraPosition;
+            }
+            set
+            {
+                QualitySettings.billboardsFaceCameraPosition = value;
+            }
+        }
+
+        public float ResolutionScalingFixedDPIFactor
+        {
+            get
+            {
+                return QualitySettings.resolutionScalingFixedDPIFactor;
+            }
+            set
+            {
+                QualitySettings.resolutionScalingFixedDPIFactor = value;
+            }
+        }
+
+        public RenderPipelineAsset RenderPipeline
+        {
+            get
+            {
+                return QualitySettings.renderPipeline;
+            }
+            set
+            {
+                QualitySettings.renderPipeline = value;
+            }
+        }
+
+        public SkinWeights SkinWeights
+        {
+            get
+            {
+                return QualitySettings.skinWeights;
+            }
+            set
+            {
+                QualitySettings.skinWeights = value;
+            }
+        }
+
+        public bool StreamingMipmapsActive
+        {
+            get
+            {
+                return QualitySettings.streamingMipmapsActive;
+            }
+            set
+            {
+                QualitySettings.streamingMipmapsActive = value;
+            }
+        }
+
+        public float StreamingMipmapsMemoryBudget
+        {
+            get
+            {
+                return QualitySettings.streamingMipmapsMemoryBudget;
+            }
+            set
+            {
+                QualitySettings.streamingMipmapsMemoryBudget = value;
+            }
+        }
+
+        public int StreamingMipmapsRenderersPerFrame
+        {
+            get
+            {
+                return QualitySettings.streamingMipmapsRenderersPerFrame;
+            }
+            set
+            {
+                QualitySettings.streamingMipmapsRenderersPerFrame = value;
+            }
+        }
+
+        public int StreamingMipmapsMaxLevelReduction
+        {
+            get
+            {
+                return QualitySettings.streamingMipmapsMaxLevelReduction;
+            }
+            set
+            {
+                QualitySettings.streamingMipmapsMaxLevelReduction = value;
+            }
+        }
+
+        public bool StreamingMipmapsAddAllCameras
+        {
+            get
+            {
+                return QualitySettings.streamingMipmapsAddAllCameras;
+            }
+            set
+            {
+                QualitySettings.streamingMipmapsAddAllCameras = value;
+            }
+        }
+
+        public int StreamingMipmapsMaxFileIORequests
+        {
+            get
+            {
+                return QualitySettings.streamingMipmapsMaxFileIORequests;
+            }
+            set
+            {
+                QualitySettings.streamingMipmapsMaxFileIORequests = value;
+            }
+        }
+
+        public int MaxQueuedFrames
+        {
+            get
+            {
+                return QualitySettings.maxQueuedFrames;
+            }
+            set
+            {
+                QualitySettings.maxQueuedFrames = value;
+            }
+        }
+
+        public string[] Names
+        {
+            get
+            {
+                return QualitySettings.names;
+            }
+        }
+
+        public ColorSpace DesiredColorSpace
+        {
+            get
+            {
+                return QualitySettings.desiredColorSpace;
+            }
+        }
+
+        public ColorSpace ActiveColorSpace
+        {
+            get
+            {
+                return QualitySettings.activeColorSpace;
+            }
+        }
+
+        // Méthodes
+
+        public void IncreaseLevel(bool applyExpensiveChanges = false)
+        {
+            QualitySettings.IncreaseLevel(applyExpensiveChanges);
+        }
+
+        public void DecreaseLevel(bool applyExpensiveChanges = false)
+        {
+            QualitySettings.DecreaseLevel(applyExpensiveChanges);
+        }
+
+        public void SetQualityLevel(int index, bool applyExpensiveChanges = true)
+        {
+            QualitySettings.SetQualityLevel(index, applyExpensiveChanges);
+        }
+
+        public void SetLODSettings(float lodBias, int maximumLODLevel, bool setDirty = true)
+        {
+            QualitySettings.SetLODSettings(lodBias, maximumLODLevel, setDirty);
+        }
+
+        public RenderPipelineAsset GetRenderPipelineAssetAt(int index)
+        {
+            return QualitySettings.GetRenderPipelineAssetAt(index);
+        }
+
+        public int GetQualityLevel()
+        {
+            return QualitySettings.GetQualityLevel();
+        }
+
+        public Object GetQualitySettings()
+        {
+            return QualitySettings.GetQualitySettings();
+        }
+
+        public void GetAllRenderPipelineAssetsForPlatform(string buildTargetGroupName, ref List<RenderPipelineAsset> renderPipelineAssets)
+        {
+            QualitySettings.GetAllRenderPipelineAssetsForPlatform(buildTargetGroupName, ref renderPipelineAssets);
+        }
+    }
+}
