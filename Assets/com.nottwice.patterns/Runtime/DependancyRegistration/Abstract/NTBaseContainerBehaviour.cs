2edabc59f4fa2b41abfa6fce39b3702ed4cb5ad8
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2024-09-08 22:54:10 +0200

    update

---
+++ Assets/com.nottwice.patterns/Runtime/DependancyRegistration/Abstract/NTBaseContainerBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace NotTwice.Patterns.DependancyRegistration.Runtime.Abstract
+{
+	public abstract class NTBaseContainerBehaviour : MonoBehaviour
+	{
+		[Required]
+		public NTDependancyContainer Container;
+
+		protected T GetService<T>()
+			where T : class
+		{
+			return Container.GetService<T>();
+		}
+	}
+}
