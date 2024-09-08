09b1117c18ea9a110aa37cfe1e2cc0e156635aff
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2024-09-09 00:11:25 +0200

    update

---
+++ Assets/com.nottwice.events/Runtime/Components/Behaviours/NTOnLoadedSceneEvent.cs
@@ -0,0 +1,38 @@
+﻿using NotTwice.Proxies.Runtime.Interfaces;
+using NotTwice.Proxies.Runtime;
+using UnityEngine.SceneManagement;
+using NotTwice.Events.Runtime.Serializables.Abstract;
+using NotTwice.Events.Runtime.ScriptableObjects.Events;
+using UnityEngine;
+
+namespace NotTwice.Events.Runtime.Components.Behaviours
+{
+	[AddComponentMenu("NotTwice/Events/Behaviours/OnUnloadSceneEvent")]
+	public class NTOnLoadedSceneEvent : NTGenericEventTypeSwitcher<NTSceneGameEvent, Scene>
+	{
+		private INTSceneManager _sceneManagerInternal;
+
+		private INTSceneManager _sceneManager
+		{
+			get
+			{
+				if (_sceneManagerInternal == null)
+				{
+					_sceneManagerInternal = new NTSceneManagerProxy();
+				}
+
+				return _sceneManagerInternal;
+			}
+		}
+
+		public void Awake()
+		{
+			_sceneManager.AddSceneLoadedEvent(OnLoadScene);
+		}
+
+		private void OnLoadScene(Scene scene, LoadSceneMode loadSceneMode)
+		{
+			Raise(scene);
+		}
+	}
+}
